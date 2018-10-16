2def5a813c4af7c1b5d691dec236d09db2afa694
Egor Uni <egoruni@isavin.fritz.box>
2018-10-16 18:44:25 +0200

    json1 problem - 8 solution

--- zas_rep_tools/src/extensions/tweet_nlp/ark_tweet_nlp/CMUTweetTagger.py
+++ zas_rep_tools/src/extensions/tweet_nlp/ark_tweet_nlp/CMUTweetTagger.py
@@ -102,15 +102,17 @@ def check_script_is_present(run_tagger_cmd=RUN_TAGGER_CMD):
         # old call - made a direct call to runTagger.sh (not Windows friendly)
         #po = subprocess.Popen([run_tagger_cmd, '--help'], stdout=subprocess.PIPE)
         #p(po.poll(), "11po.poll()")
-        #while not po.poll():
-        for answer in po.poll():
-            if not answer:
-                break
+        while not po.poll():
+        #for answer in po.poll():
+            #if not answer:
+            #    break
             #p(po.poll(), "po.poll()")
-            _1 = repr(answer)
-            _2 = type(answer)
-            print "_1= ", _1, " _2= ", _2
+            #_1 = repr(answer)
+            #_2 = type(answer)
+            #print "_1= ", _1, " _2= ", _2
+            p(po.stdout, "po.stdout")
             lines = [l for l in po.stdout]
+            p(lines,"lines")
             
         #print 
         # we expected the first line of --help to look like the following:
