873434abebefd8dd1f86287a5914f25025b0f571
Egor Uni <egoruni@isavin.fritz.box>
2018-10-16 18:35:31 +0200

    json1 problem - 8 solution

--- zas_rep_tools/src/extensions/tweet_nlp/ark_tweet_nlp/CMUTweetTagger.py
+++ zas_rep_tools/src/extensions/tweet_nlp/ark_tweet_nlp/CMUTweetTagger.py
@@ -101,10 +101,11 @@ def check_script_is_present(run_tagger_cmd=RUN_TAGGER_CMD):
         #print(po.communicate())
         # old call - made a direct call to runTagger.sh (not Windows friendly)
         #po = subprocess.Popen([run_tagger_cmd, '--help'], stdout=subprocess.PIPE)
+        p(po.poll(), "11po.poll()")
         while po.poll() is not None:
             _1 = repr(po.poll())
             _2 = type(po.poll())
-            p(  (_1,_2))
+            print "_1= ", _1, " _2= ", _2
             lines = [l for l in po.stdout]
             
         #print 
